57d48d570acc8fae18cec7a177022b9d5040aaa2
Jordan Christensen <jordan.c4922@gmail.com>
2024-03-28 10:55:34 -0600

    Initial Commit

---
+++ NESEmulator/NESEmulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NESEmulatorApp.swift
+//  NESEmulator
+//
+//  Created by Jordan Christensen on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct NESEmulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
