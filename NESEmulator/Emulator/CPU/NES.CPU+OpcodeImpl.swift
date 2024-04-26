0093d3aa75dfdd9a3398cff3fd9067ce1a7b3615
Jordan Christensen <jordan.c4922@gmail.com>
2024-04-26 11:47:24 -0600

    Implement first few opcode functions with comments

--- NESEmulator/Emulator/CPU/NES.CPU+OpcodeImpl.swift
+++ NESEmulator/Emulator/CPU/NES.CPU+OpcodeImpl.swift
@@ -1,49 +1,155 @@
+
+/// Opcodes only handle the cycle count for the current instruction, and not for fetching the opcode nor handling the addressing mode
 extension NES.CPU {
+    /// Break:
+    /// Pushes the program counter and processor status to the stack.
+    /// Loads the interrupt vector from 0xFFFE/F into the program counter.
+    /// Sets the break flag status to 1 (in both the active status and the status pushed to the stack).
+    /// - Note: This function increments the clock cycle count by 6 (instead of the expected 7) due to the run function incrementing the clock cycle when fetching the opcode
     func brk() {
         emuLogger.debug("brk")
+        
+        registers.status.setFlag(.break, to: true)
+        
+        push(UInt8((registers.programCounter >> 8) & 0xFF))
+        push(UInt8(registers.programCounter & 0xFF))
+        push(registers.status.rawValue)
+        
+        let lowByte = UInt16(memoryManager.read(from: 0xFFFE))
+        let highByte = UInt16(memoryManager.read(from: 0xFFFF))
+        
+        registers.programCounter = lowByte | (highByte << 8)
+        
+        clockCycleCount += 6
     }
     
-    func ora() {
+    /// Logical Inclusive OR:
+    /// Performs a bitwise OR operation, bit by bit, on the accumulator with the value fetched using the specified addressing mode.
+    /// - Parameter value: The value fetched using the specified addressing mode to be ORed with the accumulator
+    /// - Note: No cycles are added to `clockCycleCount` due to the run function and addressing mode functions incrementing the cycle count
+    func ora(value: UInt8) {
         emuLogger.debug("ora")
+        registers.accumulator |= value
+        updateZeroNegativeFlags()
     }
     
+    /// Stop / Jam / Kill:
+    /// An illegal opcode that indefinitely traps the CPU in an undefined state, typically referred to as T1 phase, with 0xFF on the data bus.
+    /// This operation is not used by any NES program and is implemented here as a `fatalError` to indicate an unreachable state.
     func stp() {
         emuLogger.debug("stp")
+        fatalError("Illegal opcode 'stp/kil/jam' encountered. This is an unreachable state and indicates a severe error in the emulator.")
     }
     
-    func slo() {
+    /// Shift Left Logical OR:
+    /// Shifts all bits in the value one place to the left and then ORs the accumulator with the result.
+    /// - Parameter value: The value to perform the SLO operation on
+    /// - Note: This function increments the clock cycle count by 1 (instead of the expected 5) due to the run function and addressing mode functions incrementing the cycle count
+    func slo(value: inout UInt8) {
         emuLogger.debug("slo")
+        
+        asl(value: &value)
+        ora(value: value)
     }
     
+    /// No Operation:
+    /// Burns one clock cycle (or two, including the opcode pulled from memory).
     func nop() {
         emuLogger.debug("nop")
+        
+        clockCycleCount += 1
     }
     
-    func asl() {
+    /// Accumulator Shift Left:
+    /// Shifts the accumulator left 1 bit and updates carry, zero, and negative flags.
+    /// - Note: This function increments the clock cycle count by 1 (instead of the expected 2 or 5+ based on addressing mode) due to the run function and addressing mode functions incrementing the cycle count
+    func asl(value: inout UInt8, isAccumulator: Bool = false) {
         emuLogger.debug("asl")
+        
+        // Check for carry before performing shift
+        let carryFlag = Registers.Status.Flag.carry
+        registers.status.setFlag(carryFlag, to: value & .mostSignificantBit != 0)
+        
+        value <<= 1
+        
+        updateZeroNegativeFlags(for: value)
+        
+        clockCycleCount += isAccumulator ? 1 : 2
     }
     
+    /// Push Processor Status:
+    /// Pushes the current status register (processor status) onto the stack with the break flag and bit 5 set to 1.
+    /// Updates the stack pointer.
+    /// - Note: Increments the clock cycle count by 2 (instead of the expected 3) due to the run function incrementing the cycle count
     func php() {
         emuLogger.debug("php")
+        
+        // Bit 5 is ignored in this context
+        push(registers.status.rawValue | Registers.Status.Flag.break.rawValue)
+        
+        clockCycleCount += 2
     }
     
-    func anc() {
+    /// AND with Carry:
+    /// Performs a bitwise AND between the accumulator and the specified value, and then sets the carry flag to the same value as bit 7 of the result
+    /// - Parameter value: The value to perform the AND operation with the accumulator.
+    func anc(value: UInt8) {
         emuLogger.debug("anc")
+        
+        and(value: value)
+        
+        let carryFlag = Registers.Status.Flag.carry
+        registers.status.setFlag(carryFlag, to: registers.accumulator & .mostSignificantBit != 0)
     }
     
-    func bpl() {
+    /// Branch if Positive:
+    /// If the negative flag is clear then add the relative displacement to the program counter to cause a branch to a new location
+    /// - Note: Cycle count is incremented if branch succeeds, and is incremented again if page boundary is crossed.
+    func bpl(value: Int8) {
         emuLogger.debug("bpl")
+        
+        if !registers.status.readFlag(.negative) {
+            let newAddress = UInt16(Int16(registers.programCounter) + Int16(value))
+            if (registers.programCounter & 0xFF00) != (newAddress & 0xFF00) {
+                clockCycleCount += 1 // Add cycle when crossing page boundary
+            }
+            
+            registers.programCounter = newAddress
+            clockCycleCount += 1 // Add cycle when branch is successful
+        }
     }
     
+    /// Clear Carry Flag:
+    /// - Note: This function increments the clock cycle count by 1 (instead of the expected 2) due to the run function and addressing mode functions incrementing the cycle count
     func clc() {
         emuLogger.debug("clc")
+        
+        registers.status.setFlag(.carry, to: false)
+        clockCycleCount += 1
     }
     
-    func jsr() {
+    /// Jump to Subroutine:
+    /// Pushes the address of the return point on to the stack and then sets the program counter to the target memory address.
+    /// - Note: Increments the clock cycle count by 3 (instead of the expected 6) due to the run function and addressing mode functions incrementing the cycle count
+    func jsr(value: UInt16) {
         emuLogger.debug("jsr")
+        
+        push(UInt8((registers.programCounter >> 8) & 0xFF))
+        push(UInt8(registers.programCounter & 0xFF))
+        
+        registers.programCounter = value
+        
+        clockCycleCount += 3
     }
     
-    func and() {
+    /// And:
+    /// Performs a bitwise AND operation between the accumulator and the provided value.
+    /// Updates the zero and negative flags.
+    /// - Note: Clock cycle is not incrmented as AND takes 2 cycles total, one for fetching the opcode, and one for fetching the value
+    func and(value: UInt8) {
+        registers.accumulator &= value
+        updateZeroNegativeFlags()
+        
         emuLogger.debug("and")
     }
     
