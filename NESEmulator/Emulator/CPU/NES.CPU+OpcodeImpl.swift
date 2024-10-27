e21600fd4c99fcb6a24514f55cd2f3f5ffb82889
Jordan C. <jordan.c4922@gmail.com>
2024-10-27 11:40:39 -0600

    Implement BEQ opcode function

--- NESEmulator/Emulator/CPU/NES.CPU+OpcodeImpl.swift
+++ NESEmulator/Emulator/CPU/NES.CPU+OpcodeImpl.swift
@@ -990,8 +990,22 @@ extension NES.CPU {
         clockCycleCount += 1
     }
     
-    func beq() {
+    /// Branch if Equal
+    /// If the negative flag is clear then add the relative displacement to the program counter to cause a branch to a new location.
+    /// - Note: Cycle count is incremented if branch succeeds, and is incremented again if page boundary is crossed.
+    func beq(value: UInt8) {
         emuLogger.debug("beq")
+        
+        if registers.status.readFlag(.zero) {
+            let offset = Int8(bitPattern: value)
+            let newAddress = UInt16(Int16(registers.programCounter) + Int16(offset))
+            if (registers.programCounter & 0xFF00) != (newAddress & 0xFF00) {
+                clockCycleCount += 1 // Add cycle when crossing page boundary
+            }
+            
+            registers.programCounter = newAddress
+            clockCycleCount += 1 // Add cycle when branch is successful
+        }
     }
     
     func sed() {
