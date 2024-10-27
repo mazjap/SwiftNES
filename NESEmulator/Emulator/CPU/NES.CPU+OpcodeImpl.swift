ebaaf7abfcc900e9100028651509f00061fd9b8b
Jordan C <jordan.c4922@gmail.com>
2024-10-27 02:25:01 -0600

    Implement BCS opcode function

--- NESEmulator/Emulator/CPU/NES.CPU+OpcodeImpl.swift
+++ NESEmulator/Emulator/CPU/NES.CPU+OpcodeImpl.swift
@@ -706,8 +706,22 @@ extension NES.CPU {
         clockCycleCount += 1
     }
     
-    func bcs() {
+    /// Branch if Carry Set:
+    /// If the carry flag is set then add the relative displacement to the program counter to cause a branch to a new location.
+    /// - Note: Cycle count is incremented if branch succeeds, and is incremented again if page boundary is crossed.
+    func bcs(value: UInt8) {
         emuLogger.debug("bcs")
+        
+        if registers.status.readFlag(.carry) {
+            let offset = Int8(bitPattern: value)
+            let newAddress = UInt16(Int16(registers.programCounter) + Int16(offset))
+            if (registers.programCounter & 0xFF00) != (newAddress & 0xFF00) {
+                clockCycleCount += 1 // Add cycle when crossing page boundary
+            }
+            
+            registers.programCounter = newAddress
+            clockCycleCount += 1 // Add cycle when branch is successful
+        }
     }
     
     func clv() {
